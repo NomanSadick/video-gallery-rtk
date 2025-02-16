ec89443ed08b6465719357f8c1a3ee9d5237b0b1
Noman Sadick <nomansadickjs@gmail.com>
2025-02-16 22:41:54 +0600

    create cache behavior revalidation

--- features/api/apiSlice.ts
+++ features/api/apiSlice.ts
@@ -3,12 +3,15 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:9000", // Your API base URL
+    baseUrl: "http://localhost:9000",
+    //  // Your API base URL
   }),
+  tagTypes: ["Videos"],
   endpoints: (builder) => ({
     // Fetch all videos
     getVideos: builder.query({
       query: () => "/videos",
+      providesTags: ["Videos"],
     }),
     // Fetch a specific video by ID
     getVideoById: builder.query({
@@ -27,7 +30,8 @@ export const apiSlice = createApi({
         url: `/videos`,
         method: 'POST',
         body: data,
-      }), // Endpoint to fetch video by ID
+      }), 
+      invalidatesTags: ["Videos"]// Endpoint to fetch video by ID
     }),
   }),
 });
